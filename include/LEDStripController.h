8efe9bcf1c3bbed20943fcb915e1eb60bcabec9d
Matic Knez <matic.knez82@gmail.com>
2024-07-18 19:54:51 +0200

    Modify LEDStripController

--- include/LEDStripController.h
+++ include/LEDStripController.h
@@ -3,12 +3,19 @@
 
 class LEDStripController {
 public:
+    // Constructor
+    LEDStripController(int ledCount) {}
+
+    // Virtual Destructor
     virtual ~LEDStripController() {}
 
+    // Pure virtual functions
     virtual void init() = 0;
-    virtual void clear() = 0;
     virtual void render() = 0;
-    //virtual void setBrightness(int brightness) = 0;
+
+    virtual void led_array_setColor(uint32_t* colorArray) = 0;
+    virtual void led_array_clear() = 0;
+    virtual void setBrightness(int brightness) = 0;
 };
 
 #endif // LEDSTRIPCONTROLLER_H
